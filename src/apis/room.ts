37eba88bc92b3e93d03ea706c9e80c2e6303d220
AJN <buzz781@gmail.com>
2025-03-10 01:30:10 +0900

    refactor: 코드 분리 (api)

---
+++ src/apis/room.ts
@@ -0,0 +1,15 @@
+export async function getRooms() {
+  const apiUrl =
+    process.env.NODE_ENV === 'development'
+      ? process.env.NEXT_PUBLIC_API_URL
+      : process.env.VERCEL_URL
+
+  const res = await fetch(`${apiUrl}/api/rooms`, {
+    cache: 'force-cache',
+  })
+  if (!res.ok) {
+    throw new Error('failed to fetch ssibal')
+  }
+
+  return res.json()
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { getRooms } from '@/apis/room'
 import CategoryList from '@/components/CategoryList'
 import GridLayout from '@/components/GridLayout'
 import RoomItem from '@/components/RoomList'
@@ -16,18 +17,5 @@ const Home = async () => {
     </>
   )
 }
-async function getRooms() {
-  const apiUrl =
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_API_URL
-      : process.env.VERCEL_URL
 
-  const res = await fetch(`${apiUrl}/api/rooms`, {
-    cache: 'force-cache',
-  })
-  if (!res.ok) {
-    throw new Error('failed to fetch ssibal')
-  }
-
-  return res.json()
-}
+export default Home
