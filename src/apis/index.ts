ad720f08fded6427385e849406e7b0cc5bee901d
AJN <buzz781@gmail.com>
2025-03-23 22:01:16 +0900

    fix: 빌드테스트

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,9 +4,15 @@ import { SERVER_SIDE_API_URL } from '@/constants'
  * @info 모든FAQ를 다 가져옴
  */
 export const getFaqs = async () => {
-  const res = await fetch(`${SERVER_SIDE_API_URL}/faqs`, {
-    next: { revalidate: 600 },
+  // const res = await fetch(`${SERVER_SIDE_API_URL}/faqs`, {
+  //   cache: 'force-cache',
+  // })
+
+  //빌드 테스트
+  const res = await fetch(`http://localhost:3000/api/faqs`, {
+    cache: 'force-cache',
   })
+
   if (!res.ok) {
     throw new Error('failed to fetch')
   }
